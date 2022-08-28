2558459820bcf3765bb48d4d94c3b8422eccccff
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-28 12:07:53 +0600

    task finished

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,7 +21,14 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
+
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+            } catch (NumberFormatException nfe) {
+                justWords = justWords + " "+next;
+            }
+
             // todo: complete it
         }
         System.out.println("Sum is " + sum);
